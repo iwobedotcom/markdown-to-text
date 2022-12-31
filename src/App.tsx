8c04dbd559351c6a6cbb2989e2928071446f7c0d
iwobeearthy <iwobe.aigboje@gmail.com>
2022-12-31 02:39:44 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, ChangeEvent } from "react";
+import ReactMarkdown from "react-markdown";
 
 function App() {
+  const [md, setMd] = useState<string>("## Good old markdown!");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="markdown">
+        <textarea
+          name="markdown"
+          id="markdown"
+          className="input"
+          value={md}
+          onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+            setMd(e.target.value)
+          }
+        ></textarea>
+        <article className="result">
+          <ReactMarkdown>{md}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
   );
 }
 
